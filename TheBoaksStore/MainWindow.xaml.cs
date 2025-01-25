fcb2312d4704db54a5996898eb95b460d6392679
ZohanHaqu <161150779+ZohanHaqu@users.noreply.github.com>
2025-01-25 14:27:55 -0500

    Add project files.

---
+++ TheBoaksStore/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace TheBoaksStore
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AppListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (AppListBox.SelectedItem is ListBoxItem selectedItem)
+            {
+                AppNameTextBlock.Text = selectedItem.Content.ToString();
+                AppDescriptionTextBlock.Text = "Description for " + selectedItem.Content.ToString();
+            }
+        }
+
+        private void InstallButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (AppListBox.SelectedItem is ListBoxItem selectedItem)
+            {
+                string appName = selectedItem.Tag.ToString();
+                InstallApp(appName);
+            }
+        }
+
+        private void InstallApp(string appName)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo
+            {
+                FileName = "powershell.exe",
+                Arguments = $"-NoProfile -ExecutionPolicy Bypass -Command \"Start-Process cmd -ArgumentList '/c winget install -e --id {appName} --silent' -Verb runAs\"",
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using (Process process = Process.Start(psi))
+            {
+                process.OutputDataReceived += (sender, args) => Console.WriteLine(args.Data);
+                process.ErrorDataReceived += (sender, args) => Console.WriteLine("ERROR: " + args.Data);
+                process.BeginOutputReadLine();
+                process.BeginErrorReadLine();
+                process.WaitForExit();
+            }
+        }
+    }
+}
